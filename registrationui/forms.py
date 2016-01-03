ee9d3ec6eab11790540506213aa5b22794468bd1
Ville Ranki <ville.ranki@iki.fi>
2016-01-03 22:29:52 +0200

    Value verification

--- registrationui/forms.py
+++ registrationui/forms.py
@@ -1,11 +1,34 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import re
+from django.conf import settings
+
+class UsernameField(forms.CharField):
+    def validate(self, value):
+        super(UsernameField, self).validate(value)
+	found_s = re.findall('^[\w-]+$', value)
+	valid = bool(found_s) and found_s[0] == value
+	if not valid:
+		raise ValidationError('%s contains illegal characters for username' % value)
+
+class VerificationField(forms.CharField):
+    def validate(self, value):
+        super(VerificationField, self).validate(value)
+	valid = value.lower() == settings.VERIFICATION_ANSWER
+	if not valid:
+		raise ValidationError('%s is wrong answer for verification question' % value)
 
 class NewUserForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
+    username = forms.SlugField(label='Username', max_length=100)
     first_name = forms.CharField(label='First name', max_length=100)
     last_name = forms.CharField(label='Last name', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
-    password = forms.CharField(label='Password', max_length=100)
-    password2 = forms.CharField(label='Password (again)', max_length=100)
-    antispam = forms.CharField(label='Verification question', max_length=100)
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password (again)', max_length=100, widget=forms.PasswordInput)
+    antispam = VerificationField(label='Verification question', max_length=100, help_text=settings.VERIFICATION_QUESTION)
+
+    def clean_password(self):
+        if self.data['password'] != self.data['password2']:
+            raise forms.ValidationError('Passwords are not the same')
+        return self.data['password']
 
--- registrationui/views.py
+++ registrationui/views.py
@@ -17,24 +17,23 @@ def index(request):
 	    email = form.cleaned_data['email']
 	    password = form.cleaned_data['password']
             password2 = form.cleaned_data['password2']
-	    print 'Form data:', username, first_name, last_name, email, password
 
 	    con = ldap.initialize(settings.LDAP_URL) 
-	    print 'Init:' + str(con.bind(settings.LDAP_ADMIN_CN, settings.LDAP_PASSWORD, ldap.AUTH_SIMPLE))
+	    con.bind(settings.LDAP_ADMIN_CN, settings.LDAP_PASSWORD, ldap.AUTH_SIMPLE)
 
 # Figure out largest UID (is there a better way to do this?)
 	    base_dn = settings.LDAP_USERS_CN
 	    filter = '(objectclass=person)'
 	    attrs = ['uidNumber']
 	    uidList = con.search_s( base_dn, ldap.SCOPE_SUBTREE, filter, attrs )
-	    print "Uids: " + str( uidList )
+#	    print "Uids: " + str( uidList )
 	    largestUid = 0
 	    for user in uidList:
 		uid = int(user[1]['uidNumber'][0])
 		if uid > largestUid:
 		    largestUid = uid
 
-	    print 'Largest Uid:' + str(largestUid)
+#	    print 'Largest Uid:' + str(largestUid)
 
 # Create the new user record:
 	    add_record = [
@@ -48,15 +47,19 @@ def index(request):
 	      ('homeDirectory', [str('/home/' + username)]),
 	      ('ou', ['users'])
 	    ]
-	    print 'Add: ' + str(con.add_s('uid=' + str(username) + ',' + settings.LDAP_USERS_CN, add_record))
+	    try:
+                con.add_s('uid=' + str(username) + ',' + settings.LDAP_USERS_CN, add_record)
+	    except ldap.ALREADY_EXISTS:
+		form.add_error('username', "User already exists")
 	    con.unbind()
-
-            return HttpResponseRedirect('thanks')
+	    if form.is_valid():
+	        return HttpResponseRedirect('thanks')
 
     else:
         form = NewUserForm()
-    return render(request, 'registrationui/index.html', { 'form': form } )
+
+    return render(request, 'registrationui/index.html', { 'form': form, 'settings': settings } )
 
 def thanks(request):
-    return render(request, 'registrationui/thanks.html' )
+    return render(request, 'registrationui/thanks.html', { 'settings': settings } )
 
