1b3bb8ffcd963b4dfde717a9d6842428eac6da52
Ville Ranki <ville.ranki@iki.fi>
2016-01-03 20:58:34 +0200

    Dockerizing fixes

--- dlreg/urls.py
+++ dlreg/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     url(r'^registrationui/', include('registrationui.urls')),
+    url(r'^.*$', RedirectView.as_view(url='/registrationui/', permanent=False), name='index')
 )
+
