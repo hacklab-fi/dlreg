74d397caaeb951385eac01da9ae790bcb13faeaa
vranki <ville.ranki@nomovok.com>
2016-01-20 11:45:33 +0200

    Fixed indentataion error.

--- registrationui/views.py
+++ registrationui/views.py
@@ -23,58 +23,57 @@ def index(request):
 # Python LDAP library doesn't like Unicode, so we convert everything to ascii here.
 # If you figure out a way to use Unicode, please let me know!
 
-        try:
-            username = username.encode('ascii', 'strict')
-            first_name = first_name.encode('ascii', 'strict')
-            last_name = last_name.encode('ascii', 'strict')
-            email = email.encode('ascii', 'strict')
-            password = password.encode('ascii', 'strict')
-            password2 = password2.encode('ascii', 'strict')
-
-            con = ldap.initialize(settings.LDAP_URL) 
-            con.bind(settings.LDAP_ADMIN_CN, settings.LDAP_PASSWORD, ldap.AUTH_SIMPLE)
-
-# Figure out largest UID (is there a better way to do this?)
-            base_dn = settings.LDAP_USERS_CN
-            filter = '(objectclass=person)'
-            attrs = [b'uidNumber']
-            uidList = con.search_s( base_dn, ldap.SCOPE_SUBTREE, filter, attrs )
-    #	    print "Uids: " + str( uidList )
-            largestUid = 0
-            for user in uidList:
-                uid = int(user[1]['uidNumber'][0])
-                if uid > largestUid:
-                    largestUid = uid
-
-    #	    print 'Largest Uid:' + str(largestUid)
-
-    # Create the new user record:
-            add_record = [
-              ('objectclass', [b'inetOrgPerson', b'posixAccount', b'top']),
-              ('givenName', [ first_name.encode('ascii','ignore') ]),
-              ('uidNumber', [ str(largestUid + 1)]),
-              ('gidNumber', [b'501']),
-              ('cn', [ username ] ),
-              ('uid', [ username ] ),
-              ('sn', [ last_name ] ),
-              ('mail', [ email ] ),
-              ('userpassword', [ldap_md5_crypt.encrypt(password)]),
-              ('homeDirectory', [ '/home/' + username ]),
-              ('ou', [b'users'])
-            ]
             try:
-                usercn = 'cn=' + username + ',' + settings.LDAP_USERS_CN
-                
-                print ("User cn is:" + usercn + ", " + str(type(usercn)))
-                con.add_s(usercn, add_record)
-            except ldap.ALREADY_EXISTS:
-                form.add_error('username', "User already exists")
-            con.unbind()
-            if form.is_valid():
-                return HttpResponseRedirect('thanks')
-        except UnicodeEncodeError:
-            form.add_error(None, 'Non-ascii characters inputted - use only a-z as ldap sucks with Unicode. Use Ä->A and Ö->O')
+                username = username.encode('ascii', 'strict')
+                first_name = first_name.encode('ascii', 'strict')
+                last_name = last_name.encode('ascii', 'strict')
+                email = email.encode('ascii', 'strict')
+                password = password.encode('ascii', 'strict')
+                password2 = password2.encode('ascii', 'strict')
 
+                con = ldap.initialize(settings.LDAP_URL) 
+                con.bind(settings.LDAP_ADMIN_CN, settings.LDAP_PASSWORD, ldap.AUTH_SIMPLE)
+
+    # Figure out largest UID (is there a better way to do this?)
+                base_dn = settings.LDAP_USERS_CN
+                filter = '(objectclass=person)'
+                attrs = [b'uidNumber']
+                uidList = con.search_s( base_dn, ldap.SCOPE_SUBTREE, filter, attrs )
+        #	    print "Uids: " + str( uidList )
+                largestUid = 0
+                for user in uidList:
+                    uid = int(user[1]['uidNumber'][0])
+                    if uid > largestUid:
+                        largestUid = uid
+
+        #	    print 'Largest Uid:' + str(largestUid)
+
+        # Create the new user record:
+                add_record = [
+                  ('objectclass', [b'inetOrgPerson', b'posixAccount', b'top']),
+                  ('givenName', [ first_name.encode('ascii','ignore') ]),
+                  ('uidNumber', [ str(largestUid + 1)]),
+                  ('gidNumber', [b'501']),
+                  ('cn', [ username ] ),
+                  ('uid', [ username ] ),
+                  ('sn', [ last_name ] ),
+                  ('mail', [ email ] ),
+                  ('userpassword', [ldap_md5_crypt.encrypt(password)]),
+                  ('homeDirectory', [ '/home/' + username ]),
+                  ('ou', [b'users'])
+                ]
+                try:
+                    usercn = 'cn=' + username + ',' + settings.LDAP_USERS_CN
+                    
+                    print ("User cn is:" + usercn + ", " + str(type(usercn)))
+                    con.add_s(usercn, add_record)
+                except ldap.ALREADY_EXISTS:
+                    form.add_error('username', "User already exists")
+                con.unbind()
+                if form.is_valid():
+                    return HttpResponseRedirect('thanks')
+            except UnicodeEncodeError:
+                form.add_error(None, 'Non-ascii characters inputted - use only a-z as ldap sucks with Unicode. Use Ä->A and Ö->O')
     else:
         form = NewUserForm()
 
