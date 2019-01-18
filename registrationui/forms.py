a9cedbe532f1ad55416b9f6518fb1a2296b7306b
Ville Ranki <ville.ranki@iki.fi>
2019-01-18 15:16:56 +0200

    Force only lowercase letters in username

--- registrationui/forms.py
+++ registrationui/forms.py
@@ -3,13 +3,16 @@ from django.core.exceptions import ValidationError
 import re
 from django.conf import settings
 
-class UsernameField(forms.CharField):
+class UsernameField(forms.SlugField):
     def validate(self, value):
         super(UsernameField, self).validate(value)
-        found_s = re.findall('^[\w-]+$', value)
+        found_s = re.findall(r'^[\w-]+$', value)
         valid = bool(found_s) and found_s[0] == value
         if not valid:
             raise ValidationError('%s contains illegal characters for username' % value)
+        for c in value:
+            if c.isupper():
+                raise ValidationError('Use only lowercase letters in username')
 
 class VerificationField(forms.CharField):
     def validate(self, value):
@@ -19,7 +22,7 @@ class VerificationField(forms.CharField):
             raise ValidationError('%s is wrong answer for verification question' % value)
 
 class NewUserForm(forms.Form):
-    username = forms.SlugField(label='Username', max_length=100)
+    username = UsernameField(label='Username', help_text='Lowercase letters a-z only', max_length=100)
     first_name = forms.CharField(label='First name', max_length=100)
     last_name = forms.CharField(label='Last name', max_length=100)
     email = forms.EmailField(label='E-mail', max_length=100)
